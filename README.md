e530559aa02d56b9dfbe629d5016f6a9efbbfddd
= <180476097+lucasf-nl@users.noreply.github.com>
2024-12-02 20:02:28 +0100

    Write part 2 of day 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Advent Of Code 2024, masochism edition
+# Advent Of Code 2024, self-torment edition
 
 I have decided that for every puzzle the wheel will decide what language my solution
 will be written in. The possible list of languages is: 
@@ -36,5 +36,12 @@ will be written in. The possible list of languages is:
 ## Day 1: Historian Hysteria
 
 This AoC started off with COBOL, not great, but better than some of the other options
-on the list. As I'm programming on a Mac I had to install the `gnu-cobol` package
-through Homebrew (which I probably still have to remove).
+on the list. As I'm programming on a Mac I chose GnuCOBOL which is available in Homebrew.
+The GnuCOBOL Programmer's Guide written by Gary L. Cutler and Vincent B. Coen provided all
+the COBOL knowledge needed to write day 1. Writing day 1 took about 3 hours.
+
+## Day 2: Red-Nosed Reports
+
+Rust, a sane language. I already have a lot of experience writing in Rust so this day
+would be easy, *he said, not knowing what awaited him*. This was pretty easy, but
+part 2 required some renovations.
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
                     .map(|s| s.parse::<u8>().unwrap())
                     .collect::<Vec<u8>>();
 
-                if check_report_safety(segments) {
+                if check_report_safety(segments, true) {
                     count += 1;
                 }
             },
@@ -28,34 +28,61 @@ fn main() {
     println!("Safe reports: {}", count);
 }
 
-fn check_report_safety(numbers: Vec<u8>) -> bool {
-    if numbers.len() == 0 || numbers[0] == numbers[1] {
+fn is_increasing(numbers: &Vec<u8>) -> bool {
+    let t = numbers
+        .windows(2).map(|w| w[0] <= w[1])
+        .filter(|b| *b)
+        .collect::<Vec<bool>>();
+
+    t.len() > (numbers.len() / 2)
+}
+
+fn check_report_safety(numbers: Vec<u8>, first: bool) -> bool {
+    if numbers.len() == 0 {
         return false
     };
 
-    let increasing = numbers[0] < numbers[1];
+    let increasing = is_increasing(&numbers);
+    let original_numbers = numbers.clone();
     let mut numbers = numbers.iter();
     let mut previous: &u8 = numbers.next().unwrap();
 
-    for num in numbers {
-        if previous == num {
-            return false
-        }
-
-        if previous != num && (
-            (previous >= num && increasing) ||
-                (previous <= num && !increasing)
-        ) {
-            return false
-        }
-
+    for (i, num) in numbers.enumerate() {
         let abs = previous.abs_diff(*num);
-        if abs > 3 {
-            return false
+        if abs > 3 ||
+            abs == 0 ||
+            (previous > num && increasing) ||
+            (previous < num && !increasing) {
+
+            let mut first_index_removed = original_numbers.clone();
+            first_index_removed.remove(i);
+            let mut second_index_removed = original_numbers.clone();
+            second_index_removed.remove(i + 1);
+
+            return first && (check_report_safety(first_index_removed, false) ||
+                check_report_safety(second_index_removed, false));
         }
 
         previous = num;
     }
 
     true
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::check_report_safety;
+
+    #[test]
+    fn report_safety() {
+        assert_eq!(true, check_report_safety(vec![7, 6, 4, 2, 1], true));
+        assert_eq!(false, check_report_safety(vec![1, 2, 7, 8, 9], true));
+        assert_eq!(false, check_report_safety(vec![9, 7, 6, 2, 1], true));
+        assert_eq!(true, check_report_safety(vec![1, 3, 2, 4, 5], true));
+        assert_eq!(true, check_report_safety(vec![8, 6, 4, 4, 1], true));
+        assert_eq!(true, check_report_safety(vec![1, 3, 6, 7, 9], true));
+
+        assert_eq!(true, check_report_safety(vec![2, 3, 6, 9, 8], true));
+        assert_eq!(true, check_report_safety(vec![3, 2, 6, 8, 9], true));
+    }
 }
