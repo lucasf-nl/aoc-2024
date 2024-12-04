477e791f3845645ee21f63df4c2a43dce3285c45
lucasf-nl <180476097+lucasf-nl@users.noreply.github.com>
2024-12-04 15:01:36 +0100

    Implement part 2 of day 4 in Javascript

--- day4/main.js
+++ day4/main.js
@@ -1,24 +1,36 @@
-// const input = `MMMSXXMASM
-// MSAMXMSMSA
-// AMXSXMAAMM
-// MSAMASMSMX
-// XMASAMXAMM
-// XXAMMXXAMA
-// SMSMSASXSS
-// SAXAMASAAA
-// MAMMMXMMMM
-// MXMXAXMASX`
-
 const fs = require("fs");
 
 const input = fs.readFileSync("input.txt").toString();
 
 let input_matrix = input.split("\n").map(v => v.split(""));
 let xmas_count = 0;
+let cross_mas_count = 0;
+
+const cross_mas_lookup = { "S": "M", "M": "S" }
 
 for (let y = 0; y < input_matrix.length; y++) {
     for (let x = 0; x < input_matrix[0].length; x++) {
         let char = input_matrix[y][x]
+
+        if (char === "A" &&
+            input_matrix[y - 1] !== undefined &&
+            input_matrix[y + 1] !== undefined) {
+            // char/cords is the middle of a possible X-MAS
+
+            let ltop = input_matrix[y + 1][x - 1]
+            let lbot = input_matrix[y - 1][x - 1]
+            let rtop = input_matrix[y + 1][x + 1]
+            let rbot = input_matrix[y - 1][x + 1]
+
+            if (ltop !== undefined && lbot !== undefined &&
+                rtop !== undefined && rbot !== undefined &&
+                ltop === cross_mas_lookup[rbot] && rtop === cross_mas_lookup[lbot]) {
+                console.log(ltop + "." + rtop)
+                console.log(".A.")
+                console.log(lbot + "." + rbot)
+                cross_mas_count++
+            }
+        }
 
         if (char === "X") {
             let possible_directions = get_directions(x, y, "M")
@@ -47,6 +59,7 @@ for (let y = 0; y < input_matrix.length; y++) {
 }
 
 console.log("XMAS Count: " + xmas_count)
+console.log("X-MAS count: " + cross_mas_count)
 
 function get_directions(x, y, next) {
     let directions = [];
